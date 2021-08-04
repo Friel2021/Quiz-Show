7a521400d51f41696325a4ac50ca544ce071e073
Friel2021 <87154134+Friel2021@users.noreply.github.com>
2021-08-04 12:25:22 -0600

    Update script.js
    Updated file with correct js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Gathering HTML elements for manipulation
 var quizBody = document.getElementById("quiz");
 var resultsEl = document.getElementById("result");
 var finalScoreEl = document.getElementById("finalScore");
@@ -19,74 +18,76 @@ var buttonB = document.getElementById("b");
 var buttonC = document.getElementById("c");
 var buttonD = document.getElementById("d");
 
-// Quiz question object
 var quizQuestions = [
   {
-    question: "What is the JSON action to parse an object",
-    choiceA: "As many as you want",
-    choiceB: "3",
-    choiceC: "1",
-    choiceD: "128",
-    correctAnswer: "c",
-  },
-  {
-    question: "What does DOM stand for?",
-    choiceA: "Document Object Model",
-    choiceB: "Display Object Management",
-    choiceC: "Digital Ordinance Model",
-    choiceD: "Desktop Oriented Mode",
+    question:
+      "Which pitcher led the majors in complete games in the 2000s (2000-2009)?",
+    choiceA: "Roy Halladay",
+    choiceB: "Justin Verlander",
+    choiceC: "CC Sabathia",
+    choiceD: "Cliff Lee",
     correctAnswer: "a",
   },
   {
-    question: "What is used primarily to add styling to a web page?",
-    choiceA: "HTML",
-    choiceB: "CSS",
-    choiceC: "Python",
-    choiceD: "React.js",
-    correctAnswer: "b",
+    question: "Which American League team has the most World Series losses?",
+    choiceA: "A's",
+    choiceB: "Red Sox",
+    choiceC: "Yankees",
+    choiceD: "Tigers",
+    correctAnswer: "c",
   },
   {
-    question: "What HTML tags are JavaScript code wrapped in?",
-    choiceA: "&lt;div&gt;",
-    choiceB: "&lt;link&gt;",
-    choiceC: "&lt;head&gt;",
-    choiceD: "&lt;script&gt;",
+    question:
+      "Which player has the most extra base hits in the history of World Series play?",
+    choiceA: "Mickey Mantle",
+    choiceB: "Yogi Berra",
+    choiceC: "Bernie Williams",
+    choiceD: "Bobby Richardson",
+    correctAnswer: "a",
+  },
+  {
+    question:
+      "Which player has the most career home runs without ever winning an MVP award?",
+    choiceA: "Eddy Murray",
+    choiceB: "Jimmie Foxx",
+    choiceC: "Hank Aaron",
+    choiceD: "Jim Thome",
     correctAnswer: "d",
   },
   {
-    question: "When is localStorage data cleared?",
-    choiceA: "No expiration time",
-    choiceB: "On page reload",
-    choiceC: "On browser close",
-    choiceD: "On computer restart",
-    correctAnswer: "a",
-  },
-  {
-    question: "What does WWW stand for?",
-    choiceA: "Web World Workings",
-    choiceB: "Weak Winter Wind",
-    choiceC: "World Wide Web",
-    choiceD: "Wendy Wants Waffles",
+    question: "What pitcher holds the record for consecutive complete games?",
+    choiceA: "Cy Young",
+    choiceB: "Roger Clemens",
+    choiceC: "Jack Taylor",
+    choiceD: "Roy Halladay",
     correctAnswer: "c",
   },
   {
-    question: "What HTML attribute references an external JavaScript file?",
-    choiceA: "href",
-    choiceB: "src",
-    choiceC: "class",
-    choiceD: "index",
+    question: "Which manager struck out in his first and only MLB at-bat?",
+    choiceA: "Bobby Cox",
+    choiceB: "Sparky Anderson",
+    choiceC: "John McGraw",
+    choiceD: "Walter Aston",
+    correctAnswer: "d",
+  },
+  {
+    question:
+      "Which player is at third base in Abbott & Costello's Who's on First routine?",
+    choiceA: "Who?",
+    choiceB: "I don't know",
+    choiceC: "What?",
+    choiceD: "Tomorrow",
     correctAnswer: "b",
   },
 ];
-// Other global variables
+
 var finalQuestionIndex = quizQuestions.length;
 var currentQuestionIndex = 0;
-var timeLeft = 55;
+var timeLeft = 35;
 var timerInterval;
 var score = 0;
 var correct;
 
-// This function cycles through the object array containing the quiz questions to generate the questions and answers.
 function generateQuizQuestion() {
   gameoverDiv.style.display = "none";
   if (currentQuestionIndex === finalQuestionIndex) {
@@ -100,16 +101,14 @@ function generateQuizQuestion() {
   buttonD.innerHTML = currentQuestion.choiceD;
 }
 
-// Start Quiz function starts the TimeRanges, hides the start button, and displays the first quiz question.
 function startQuiz() {
   gameoverDiv.style.display = "none";
   startQuizDiv.style.display = "none";
   generateQuizQuestion();
 
-  //Timer
   timerInterval = setInterval(function () {
     timeLeft--;
-    quizTimer.textContent = "Time Remaining: " + timeLeft;
+    quizTimer.textContent = "Time left: " + timeLeft;
 
     if (timeLeft === 0) {
       clearInterval(timerInterval);
@@ -118,7 +117,7 @@ function startQuiz() {
   }, 1000);
   quizBody.style.display = "block";
 }
-// This function is the end page screen that displays your score after either completeing the quiz or upon timer run out
+
 function showScore() {
   quizBody.style.display = "none";
   gameoverDiv.style.display = "flex";
@@ -128,8 +127,6 @@ function showScore() {
     "You got " + score + " out of " + quizQuestions.length + " correct!";
 }
 
-// On click of the submit button, we run the function highscore that saves and stringifies the array of high scores already saved in local stoage
-// as well as pushing the new user name and score into the array we are saving in local storage. Then it runs the function to show high scores.
 submitScoreBtn.addEventListener("click", function highscore() {
   if (highscoreInputName.value === "") {
     alert("Initials cannot be blank");
@@ -154,7 +151,6 @@ submitScoreBtn.addEventListener("click", function highscore() {
   }
 });
 
-// This function clears the list for the high scores and generates a new high score list from local storage
 function generateHighscores() {
   highscoreDisplayName.innerHTML = "";
   highscoreDisplayScore.innerHTML = "";
@@ -169,7 +165,6 @@ function generateHighscores() {
   }
 }
 
-// This function displays the high scores page while hiding all of the other pages from
 function showHighscore() {
   startQuizDiv.style.display = "none";
   gameoverDiv.style.display = "none";
@@ -180,24 +175,21 @@ function showHighscore() {
   generateHighscores();
 }
 
-// This function clears the local storage of the high scores as well as clearing the text from the high score board
 function clearScore() {
   window.localStorage.clear();
   highscoreDisplayName.textContent = "";
   highscoreDisplayScore.textContent = "";
 }
 
-// This function sets all the variables back to their original values and shows the home page to enable replay of the quiz
 function replayQuiz() {
   highscoreContainer.style.display = "none";
   gameoverDiv.style.display = "none";
   startQuizDiv.style.display = "flex";
-  timeLeft = 76;
+  timeLeft = 35;
   score = 0;
   currentQuestionIndex = 0;
 }
 
-// This function checks the response to each answer
 function checkAnswer(answer) {
   correct = quizQuestions[currentQuestionIndex].correctAnswer;
 
@@ -206,19 +198,17 @@ function checkAnswer(answer) {
     alert("That Is Correct!");
     currentQuestionIndex++;
     generateQuizQuestion();
-    //display in the results div that the answer is correct.
   } else if (
     answer !== correct &&
     currentQuestionIndex !== finalQuestionIndex
   ) {
     alert("That Is Incorrect.");
+    timeLeft -=5;
     currentQuestionIndex++;
     generateQuizQuestion();
-    //display in the results div that the answer is wrong.
   } else {
     showScore();
   }
 }
 
-// This button starts the quiz!
 startQuizButton.addEventListener("click", startQuiz);
